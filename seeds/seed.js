f8c4536b5dc3cba3793fee4cb0fd358e3572d753
ThinkK1ng <cla.givens@gmail.com>
2023-01-26 20:11:57 -0600

    fixed seeds and index files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const sequelize = require("../config/connection");
-const { User } = require("../models/user");
+const { User } = require("../models");
 
 const userData = require("./userData.json");
 
