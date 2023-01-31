31d15fcc621ae3c06f593fa38e5ba0bf1da34248
hmustain <hunter@bunkerbranding.com>
2023-01-31 15:20:33 -0600

    added comments to trackRoute.js file explaining what the code is doing

--- controllers/api/trackRoute.js
+++ controllers/api/trackRoute.js
@@ -27,18 +27,18 @@ router.get("/", async (req, res) => {
         console.log("Search route hit")
         const data = await api.clientCredentialsGrant();
         api.setAccessToken(data.body.access_token);
-
+// defining const for query, type, and limit w/ the last 2 having defaults incase the user doesn't submit info
         const query = req.body.q;
         const type = req.body.type || "track";
         const limit = req.body.limit || 10;
-
+// if no query entered return error
         if (!query) {
             return res.status(400).json({ message: "Search query 'q' is missing" });
         }
-
+// else store the returned information in const searchResult
         const searchResult = await api.searchTracks(query, { type, limit });
         const tracks = searchResult.body.tracks.items;
-        
+// maps over the array of tracks and for each track creates a new object with desired properties artist name, album name, track name, album photo, track preview
         const filteredTracks = tracks.map(track => ({
             artist: track.artists[0].name,
             album: track.album.name,
@@ -46,7 +46,7 @@ router.get("/", async (req, res) => {
             albumPhoto: track.album.images[0].url,
             preview: track.preview_url
         }));
-        
+//  if good response return the const filtered tracks in json format else return error
         res.json({ tracks: filteredTracks });
     } catch (error) {
         console.error(error);
