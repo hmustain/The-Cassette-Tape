803a0a18175780c0a88cf9e57b6a4417949a4dce
hmustain <hunter@bunkerbranding.com>
2023-01-30 19:09:32 -0600

    added PUT request for event

--- controllers/api/eventRoute.js
+++ controllers/api/eventRoute.js
@@ -23,3 +23,30 @@ router.get('/:id', async (req, res) => {
         res.status(500).json({ message: "Error (500) cannot perform GET request"});
     }
 });
+
+// POST a new event
+router.post('/', async (req, res) => {
+    try {
+        const newEvent = await Event.create(req.body);
+        res.json(newEvent);
+    } catch (err) {
+        res.status(500).json({ message: "Error (500) cannot perform POST request"});
+    }
+});
+
+// PUT (update) an existing event
+router.put('/:id', async (req, res) => {
+    try {
+        const updateEvent = await Event.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!updateEvent[0]) {
+            return res.status(404).json({ message: "Event not found"});
+        }
+        res.json(updateEvent);
+    } catch (err) {
+        res.status(500).json({ message: "Error (500) cannot perform PUT request"});
+    }
+});
