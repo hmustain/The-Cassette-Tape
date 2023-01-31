1e3218c8ef976e2602efa26a74204279fa8df8c0
hmustain <hunter@bunkerbranding.com>
2023-01-30 19:16:51 -0600

    removed commented out code for router.use/events

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const eventRoute = require('./eventRoute');
 const userRoute = require('./userRoute');
 const trackRoute = require('./trackRoute');
 
-// router.use('/events', eventRoute);
+router.use('/events', eventRoute);
 router.use('/users', userRoute);
 // router.use('/tracks', trackRoute);
 
