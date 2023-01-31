ececc6d57d29c3f4baf29d8f8b6ced220548c8ec
hmustain <hunter@bunkerbranding.com>
2023-01-31 11:18:10 -0600

    new get route to get tracks based on what the user enters is partly working, still a bug where limiting results is not functioning as intended

--- controllers/api/trackRoute.js
+++ controllers/api/trackRoute.js
@@ -6,18 +6,41 @@ const api = new spotifyApi({
   clientSecret: process.env.SPOTIFY_SECRET
 });
 
-// GET route to return tracks with "love"
+// // GET route to return tracks with "love"
+// router.get("/", async (req, res) => {
+//     try {
+//     const data = await api.clientCredentialsGrant();
+//     api.setAccessToken(data.body['access_token']);
+//     const searchResult = await api.searchTracks("Love");
+//     const tracks = searchResult.body.tracks.items;
+//     res.json({ tracks });
+//     } catch (err) {
+//     console.error(err);
+//     res.render("error", { error: err });
+//     }
+//     });
+
+
+// GET route to return tracks with whatever the user inputs
 router.get("/", async (req, res) => {
     try {
-    const data = await api.clientCredentialsGrant();
-    api.setAccessToken(data.body['access_token']);
-    const searchResult = await api.searchTracks("Love");
-    const tracks = searchResult.body.tracks.items;
-    res.json({ tracks });
-    } catch (err) {
-    console.error(err);
-    res.render("error", { error: err });
+        console.log("Search route hit")
+      const data = await api.clientCredentialsGrant();
+      api.setAccessToken(data.body.access_token);
+      console.log("Request body:", req.body);
+      const query = req.body.q;
+      if (!query) {
+        res.status(400).json({ message: "Search query 'q' is missing" });
+        return;
+      }
+      const searchResult = await api.searchTracks(query);
+      const tracks = searchResult.body.tracks.items;
+      res.json({ tracks });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Failed to retrieve tracks", error });
     }
-    });
+  });
+  
 
 module.exports = router;
