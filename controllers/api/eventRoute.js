738f2c2721b2d487ad59da5f99bc1d1fc5c4ab86
hmustain <hunter@bunkerbranding.com>
2023-01-30 18:57:50 -0600

    added GET req by :id for the eventRoute

--- controllers/api/eventRoute.js
+++ controllers/api/eventRoute.js
@@ -10,3 +10,16 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: "Error (500) cannot perform GET request"});
     }
 });
+
+// GET event by :ID
+router.get('/:id', async (req, res) => {
+    try {
+        const event = await Event.findByPk(req.params.id);
+        if (!event) {
+            return res.status(404).json({ message: "Event not found"});
+        }
+        res.json(event);
+    } catch (err) {
+        res.status(500).json({ message: "Error (500) cannot perform GET request"});
+    }
+});
