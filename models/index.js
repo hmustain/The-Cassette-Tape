77c59bbe39fc50c643276f2895c2de90639cb72d
hmustain <hunter@bunkerbranding.com>
2023-02-05 18:16:41 -0600

    updating user.js folder to User.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // References the models for this application
 // changing the name of the user folder to User.js to match the requirement for heroku -- from user.js to User.js
-const User = require("./User");
+const User = require("./user");
 const Event = require("./Event");
 const UserEvent = require("./UserEvent");
 const Song = require("./Song");
