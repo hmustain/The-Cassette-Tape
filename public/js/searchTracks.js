ddf4a3e26bdf7d9029aca10b6f34a753d2fa1891
Hunter Mustain <109978698+hmustain@users.noreply.github.com>
2023-02-06 18:09:13 -0600

    added process.env.PORT to fetch

--- public/js/searchTracks.js
+++ public/js/searchTracks.js
@@ -5,9 +5,9 @@ const searchTracks = async (event) => {
     const limit = document.getElementById("track-limit").value;
 
     try {
-        console.log(`http://localhost:3001/api/tracks/search?q=${query}&type=${type}&limit=${limit}`);
+        console.log(`http://localhost:${process.env.PORT || 3001}/api/tracks/search?q=${query}&type=${type}&limit=${limit}`);
 
-        const response = await fetch(`http://localhost:3001/api/tracks/search?q=${query}&type=${type}&limit=${limit}`, {
+        const response = await fetch(`http://localhost:${process.env.PORT || 3001}/api/tracks/search?q=${query}&type=${type}&limit=${limit}`, {
             method: "GET"
         });
         const data = await response.json();
